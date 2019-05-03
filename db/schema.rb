632920f9576d176ad5e1b7fc509b3ce902e3c170
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-05-03 15:53:52 +0900

    create_ItemTable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_113438) do
+ActiveRecord::Schema.define(version: 2019_05_03_061529) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "detail", null: false
+    t.text "image", null: false
+    t.string "size", null: false
+    t.string "condition"
+    t.string "shipment_day", null: false
+    t.string "delivery_fee", null: false
+    t.string "prefecture", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
