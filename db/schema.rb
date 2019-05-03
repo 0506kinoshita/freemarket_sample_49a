78f192dc09cd6abbd6db46892ad4103215c666b4
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-05-03 19:11:56 +0900

    generate_model

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,20 @@ ActiveRecord::Schema.define(version: 2019_05_03_062430) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "detail", null: false
+    t.text "image", null: false
+    t.string "size", null: false
+    t.string "condition"
+    t.string "shipment_day", null: false
+    t.string "delivery_fee", null: false
+    t.string "prefecture", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
