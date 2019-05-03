2e95cc5cbe6731957f27ccd710da9a3976291015
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-05-03 20:23:53 +0900

    Tweak

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :items_like, dependent: :destroy
-  has_many :comments dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :item_categories
   has_many :categories, through: :item_categories
   has_many :ordered_items
