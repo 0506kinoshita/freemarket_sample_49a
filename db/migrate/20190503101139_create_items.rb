74c57733f998b6e28692f764fc0aefb91f4b2cfc
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-05-03 19:30:27 +0900

    edit_migration_file

--- db/migrate/20190503101139_create_items.rb
+++ db/migrate/20190503101139_create_items.rb
@@ -1,7 +1,16 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string   :name, null: false
+      t.integer  :price, null: false
+      t.text     :detail, null: false
+      t.text     :image, null: false
+      t.string   :size, null: false
+      t.string   :condition
+      t.string   :shipment_day, null: false
+      t.string   :delivery_fee, null: false
+      t.string   :prefecture, null: false
+      t.references :user, foregin_key: true
       t.timestamps
     end
   end
