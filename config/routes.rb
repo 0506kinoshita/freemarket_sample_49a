a98b698688028098f51dc47907da2dd5c8626705
yusuke39 <hiranoyusuke39@gmail.com>
2019-05-03 14:15:43 +0900

    edit_routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    collection do
+      get :confirm
+    end
+  end
   root 'items#index'
-  resources :items
-  resources :users, :only => [:index,:show]
+  resources :items 
+  resources :users, only: [:index,:show,:new] do
+  collection do
+    get :confirm
+  end
+  end
 end
