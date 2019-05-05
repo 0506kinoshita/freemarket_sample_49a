328dd8a5b18908f51cf1426e6a44865092be7df5
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-05-05 19:59:59 +0900

    edit_item_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,22 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.order(created_at: :desc) .limit(4)
-    # binding.pry
   end
 
+  def new
+    @item = Item.new 
+  end
+
+  def create
+    # binding.pry
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :detail, :category, :condition, :delivery_fee, :prefecture, :shipment_day, :price)
+  end
 end
