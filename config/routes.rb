ca40e9a12d48ef540bec3e21a4a607232593bcd9
yusuke39 <hiranoyusuke39@gmail.com>
2019-05-08 20:34:36 +0900

    edit_viewfils_and_controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations'
+  devise_for :users, controllers: {
+    registrations: 'devise/registrations',
+    sessions: 'devise/sessions'
   }
    devise_scope :user do
-    get 'users/index', to: 'users/registrations#index'
+    get 'users/index', to: 'devise/registrations#index'
+    get 'users/destroy', to: 'devise/sessions#destroy'
   end 
   root 'items#index'
   resources :items 
