f9c1c030b3be481dff7249f49359fad9f267763f
TTKeN <kentarou727@gmail.com>
2019-05-09 12:36:24 +0900

    Modify files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,20 +2,19 @@ class ItemsController < ApplicationController
 
   WOMAN    = 1
   MAN      = 2
-  BabyKids = 3
-  Interior = 4
-  Books    = 5
-  Hobys    = 6
-  Cosme    = 7
-  
+  BABYKIDS = 3
+  INTERIOR = 4
+  BOOKS    = 5
+  HOBYS    = 6
+  COSME    = 7
 
   def index
     @item = Item.order("created_at DESC").limit(4)
     @items_for_woman = Category.get_category_items(WOMAN)
     @items_for_man = Category.get_category_items(MAN)
-    @items_for_BabyKids = Category.get_category_items(BabyKids)
-    @items_for_Hobys = Category.get_category_items(Hobys)
-    @items_for_Cosme = Category.get_category_items(Cosme)
+    @items_for_babykids = Category.get_category_items(BABYKIDS)
+    @items_for_hobys = Category.get_category_items(HOBYS)
+    @items_for_cosme = Category.get_category_items(COSME)
   end
 
   def new
@@ -35,5 +34,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :detail, :category, :condition, :delivery_fee, :prefecture, :shipment_day, :price, :category_id)
   end
-  
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -7,16 +7,9 @@ class Category < ApplicationRecord
     category = Category.find(id)
     categories = [
       category,
-        # cat1
       category.children,
-        # cat2
       category.children.map { |category| category.children }
-        # cat3  mapでchildrenのchildrenを配列に入れる
     ].flatten.compact
-          #配列の中の配列を親の配列にまとめる、一つにする nilを除く
-          # cat1,cat2,cat3の配列ができる
     categories.map { |category| category.items }.flatten.sort_by{|i| i.created_at}.reverse
-            # /cat1,cat2,cat3|からitemをmapで抽出する。作成順に並び替える。逆順にする
-            # [新着順のcat1,cat2,cat3]
   end
 end
