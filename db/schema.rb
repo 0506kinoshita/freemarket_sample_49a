66fa243293d21ec6b4ecdcb7b6b398a45a126242
0506kinoshita <irohas.irohas@gmail.com>
2019-05-09 14:38:32 +0900

    initial_commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2019_05_09_041921) do
+=======
+ActiveRecord::Schema.define(version: 2019_05_08_082300) do
+>>>>>>> Stashed changes
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip_code", null: false
