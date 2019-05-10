7152a5437e17d7464b429e58a992bf70141f54ca
TTKeN <kentarou727@gmail.com>
2019-05-10 14:32:16 +0900

    Modify files

--- app/models/category.rb
+++ app/models/category.rb
@@ -13,6 +13,5 @@ class Category < ApplicationRecord
     categories.map { |category| category.items }.flatten.sort_by{|i| i.created_at}.reverse
   end
 
-  scope :skim, -> (type) { get_category_items(type).first(4)}
-
+  scope :skim, ->(type) { get_category_items(type).first(4) }
 end
