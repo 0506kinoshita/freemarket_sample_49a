ac7d875088c1699f7cbcbe75cacdb993131a64ec
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-05-11 16:30:08 +0900

    Item_edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_05_10_053940) do
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip_code", null: false
@@ -67,13 +65,11 @@ ActiveRecord::Schema.define(version: 2019_05_10_053940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "size"
-    t.bigint "category_id"
     t.integer "prefecture_id"
-
-    t.integer "user_id"
-
+    t.bigint "category_id"
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_items_on_category_id"
-
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -92,13 +88,12 @@ ActiveRecord::Schema.define(version: 2019_05_10_053940) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.text "profile"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "users"
   add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
 end
