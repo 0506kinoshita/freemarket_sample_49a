f76421b68a5ab7699508c62aa7446137268cd186
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-05-12 20:23:38 +0900

    create_SnsCredential_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_053940) do
+ActiveRecord::Schema.define(version: 2019_05_12_035243) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip_code", null: false
@@ -72,6 +72,15 @@ ActiveRecord::Schema.define(version: 2019_05_10_053940) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "uid"
+    t.string "provider"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -96,4 +105,5 @@ ActiveRecord::Schema.define(version: 2019_05_10_053940) do
   add_foreign_key "addresses", "users"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
+  add_foreign_key "sns_credentials", "users"
 end
