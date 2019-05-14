20bd4e389f4d43045f34c41657f894965a93358a
TTKeN <kentarou727@gmail.com>
2019-05-14 14:35:43 +0900

    show hamburger menu detail
    ハンバーガーメニューのデータを本番用に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production? #本番環境でのみBasic認証を行う
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller? 
+  before_action :set_category
 
   def configure_permitted_parameters
     added_attrs = [:nickname, :mail_address, :password, :password_confirm, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :phone_number]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_updata, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+
+  def set_category
+    @categorys = Category.where(parent_id: nil)
   end
 
   private
