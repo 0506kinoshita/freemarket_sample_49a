b1a43d09be2d54609bfc6c38157105e07a647a00
TTKeN <kentarou727@gmail.com>
2019-05-16 16:18:42 +0900

    Add brand_data
    ブランドcsv追加
    ブランドタグからブランド表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production? #本番環境でのみBasic認証を行う
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller? 
-  before_action :set_category
+  before_action :set_category, :set_brand
 
   def configure_permitted_parameters
     added_attrs = [:nickname, :mail_address, :password, :password_confirm, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :phone_number]
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
 
   def set_category
     @categorys = Category.where(parent_id: nil)
+  end
+
+  def set_brand
+    @brands = Brand.order("created_at DESC").where.not(id: 1)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ require "csv"
 CSV.foreach('db/category_table_data.csv') do |row|
   Category.create(name: row[0], parent_id: row[1])
 end
+
+CSV.foreach('db/brands_table_data.csv') do |row|
+  Brand.create(name: row[0])
+end
